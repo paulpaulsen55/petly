5afbfd84762ff3767bfdc6d873df83ef761ce626
paulpaulsen55 <paulpaulsenx@gmail.com>
2024-04-07 18:27:57 +0200

    Update imprint and privacy pages

--- frontend/src/routes/imprint/+page.svelte
+++ frontend/src/routes/imprint/+page.svelte
@@ -1,2 +1,25 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<main class="grid gap-5">
+    <p>
+        Petly Co. <br />
+        1. Musterstraße <br />
+        12345 Test
+    </p>
+
+    <p>Telefonnr.: +972 (3) 545-4900</p>
+
+    <p>Fax: +123 456 789</p>
+
+    <p>E-Mail-Adresse: support@petly.com</p>
+
+    <p>
+        Vertreten durch den Aufsichtsrat:
+        https://investors.wix.com/board-of-directors
+    </p>
+
+    <p>petly Co. ist ein in Deutschland eingetragenes Unternehmen.</p>
+
+    <p>Handelsregisternummer: 123456789</p>
+
+    <p>Mehrwertsteuer-Identifikationsnummer: 123456789</p>
+    <span class="my-[23rem]"></span>
+</main>
--- frontend/src/routes/privacy/+page.svelte
+++ frontend/src/routes/privacy/+page.svelte
@@ -1,2 +1,86 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<main class="grid gap-5">
+    <h1>Datenschutzerklärung</h1>
+
+    <p>Verantwortlich im Sinne der Datenschutzgesetzes:</p>
+
+    <p>
+        Petly Co. <br />
+        1. Musterstraße <br />
+        12345 Test
+    </p>
+    <h2>Datenschutz</h2>
+
+    <p>
+        Als Webseitenbetreiber nehmen wir den Schutz aller persönlichen Daten
+        sehr ernst. Alle personenbezogenen Informationen werden vertraulich und
+        gemäß den gesetzlichen Vorschriften behandelt, wie in dieser
+        Datenschutzerklärung erläutert.
+    </p>
+
+    <p>
+        Unserer Webseite kann selbstverständlich genutzt werden, ohne dass Sie
+        persönliche Daten angeben. Wenn jedoch zu irgendeinem Zeitpunkt
+        persönliche Daten wie z.B. Name, Adresse oder E-Mail abgefragt werden,
+        wird dies auf freiwilliger Basis geschehen. Niemals werden von uns
+        erhobene Daten ohne Ihre spezielle Genehmigung an Dritte weitergegeben.
+    </p>
+
+    <p>
+        Datenübertragung im Internet, wie zum Beispiel über E-Mail, kann immer
+        Sicherheitslücken aufweisen. Der komplette Schutz der Daten ist im
+        Internet nicht möglich.
+    </p>
+
+    <h2>Auskunft, Löschung, Sperrung</h2>
+
+    <p>
+        Zu jedem Zeitpunkt können Sie sich über die personenbezogenen Daten,
+        deren Herkunft und Empfänger und den Nutzen der Datenverarbeitung
+        informieren und unentgeltlich eine Korrektur, Sperrung oder Löschung
+        dieser Daten verlangen. Bitte nutzen Sie dafür die im Impressum
+        angegebenen Kontaktwege. Für weitere Fragen zum Thema stehen wir Ihnen
+        ebenfalls jederzeit zur Verfügung.
+    </p>
+
+    <h2>Server-Log-Files</h2>
+
+    <p>
+        Der Seiten-Provider erhebt und speichert automatisch Daten in Server-Log
+        Files, die von Ihrem Browser an uns übermittelt werden. Diese Daten
+        enthalten:
+    </p>
+    <ul>
+        <li>Browsertyp/ Browserversion</li>
+        <li>Betriebssystem des Rechners</li>
+        <li>Referrer URL</li>
+        <li>Hostname des zugreifenden Rechners</li>
+        <li>Uhrzeit der Serveranfrage</li>
+    </ul>
+
+    <p>
+        Diese Daten sind nicht personenbezogen. Es erfolgt keine Zusammenführung
+        dieser Daten mit anderen Datenquellen. Wenn uns konkrete Anhaltspunkte
+        für eine rechtswidrige Nutzung bekannt werden behalten wir uns das Recht
+        vor, diese Daten nachträglich zu überprüfen.
+    </p>
+
+    <h2>Änderung der Datenschutzbestimmungen</h2>
+
+    <p>
+        Unsere Datenschutzerklärung können in unregelmäßigen Abständen angepasst
+        werden, damit sie den aktuellen rechtlichen Anforderungen entsprechen
+        oder um Änderungen unserer Dienstleistungen umzusetzen, z. B. bei der
+        Einfügung neuer Angebote. Für Ihren nächsten Besuch gilt dann
+        automatisch die neue Datenschutzerklärung.
+    </p>
+
+    <h2>Kontakt zum Datenschutzmitarbeiter</h2>
+
+    <p>
+        Für Fragen zum Datenschutz schicken Sie uns bitte eine Nachricht an
+        support@petly.com mit dem Betreff „Datenschutz“.
+    </p>
+
+    Diese Widerrufsbelehrung Seite wurde bei datenschutzerklärungmuster.de
+    erstellt.
+</main>
